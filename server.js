f878c2dcd2c95e60169585e8f228762029c3efde
andres.cuervo@webfg.com <andres.cuervo@webfg.com>
2022-07-19 18:49:12 +0200

    Object Types

--- server.js
+++ server.js
@@ -3,32 +3,39 @@ var {graphqlHTTP} = require('express-graphql');
 var { buildSchema } = require('graphql');
 
 var schema = buildSchema(`
+    type RandomDie {
+        numSides: Int!
+        rollOnce: Int!
+        roll(numRolls: Int!): [Int]
+    }
+
     type Query {
-        quoteOfTheDay: String
-        random: Float!
-        rollThreeDice: [Int]
-        rollDice(numDice: Int!, numSides: Int): [Int]
+        getDie(numSides:Int): RandomDie    
     }
 `);
 
-var root = {
-    quoteOfTheDay: () => {
-        return Math.random() < 0.5 ? 'Take it easy' : 'Salvation lies within';
-    },
-    random: () => {
-        return Math.random();
-    },
-    rollThreeDice: () => {
-        return [1, 2, 3].map(_ => 1 + Math.floor(Math.random() * 6));
-    },
-    rollDice: ({ numDice, numSides }) => {
+class RandomDie {
+    constructor(numSides) {
+        this.numSides = numSides;
+    }
+
+    rollOnce() {
+        return 1 + Math.floor(Math.random() * this.numSides);
+    }
+
+    roll({ numRolls }) { 
         var output = [];
-        for (var i = 0; i < numDice; i++) {
-            output.push(1 + Math.floor(Math.random() * (numSides || 6)));
+        for (var i = 0; i < numRolls; i++) {
+            output.push(this.rollOnce());
         }
         return output;
     }
-    
+}
+
+var root = {
+    getDie: ({ numSides }) => {
+        return new RandomDie(numSides || 6);
+    }
 };
 
 var app = express();
