b6e311a8173c8028d67e9c3642e91db88918ce34
andres.cuervo@webfg.com <andres.cuervo@webfg.com>
2022-07-19 18:14:13 +0200

    Running an Express GraphQL Server

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
-var { graphql, buildSchema } = require('graphql');
+var express = require('express');
+var {graphqlHTTP} = require('express-graphql');
+var { buildSchema } = require('graphql');
 
 var schema = buildSchema(`
     type Query {
@@ -6,17 +8,18 @@ var schema = buildSchema(`
     }
 `);
 
-var rootValue = {
+var root = {
     hello: () => {
         return 'Hello world!';
     },
 };
 
 
-graphql({
-    schema,
-    source: '{ hello }',
-    rootValue
-}).then((response) => {
-    console.log(response);
-});
+var app = express();
+app.use('/graphql', graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true,
+}));
+app.listen(4000);
+console.log('Running a GraphQL API server at localhost:4000/graphql');
